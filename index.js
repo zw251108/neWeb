dd9246622ae0728b620500493803b52aba72ae89
zw251108 <zw150026@163.com>
2015-04-01 22:54:34 +0800

    86

--- index.js
+++ index.js
@@ -121,6 +121,11 @@ web.get('/', function(req, res){
 	}) );
 	res.end();
 });
+// 测试用
+web.get('/getJson', function(req, res){
+	res.send('{"title":"这是一个 JSON ","data":[{},{},{},{}]}');
+	res.end()
+});
 
 //----- 加载模块 -----
 //require('./module/blog.js')(    web, db, socket, metro);  // 加载模块 blog
