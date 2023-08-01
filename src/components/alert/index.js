a57efd21e920e237bb5450b8acbc6967fb125f9b
zw251108 <zw150026@163.com>
2023-08-01 17:15:41 +0800

    微调 UI

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -4,9 +4,9 @@ function Alert({title, content, cb}){
 			<div className="module_title">{title}</div>
 			<div className="module_content">
 				<div className="alert_content">{content}</div>
-				<div className="alert_op">
-					<button className="btn" onClick={cb}>确定</button>
-				</div>
+			</div>
+			<div className="alert_op">
+				<button className="btn" onClick={cb}>确定</button>
 			</div>
 		</div>
 	</div>);
